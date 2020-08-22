121422e8d446e64c3bb42939145658381ebd79c1
Maciej <maciej.skwara2@gmail.com>
2020-08-22 21:42:46 +0200

    mirror for Y axis

--- fanuc_mirror.py
+++ fanuc_mirror.py
@@ -7,37 +7,82 @@ file2 = open("dane_mirror.txt", "w")
 # Zmiana znaku przy: Y, W, R
 # Y:
 pattern_Y = re.compile('\s[Y]\s\=\s{2,5}[-]{0,1}\d{0,4}\.')
-
 # W:
-pattern_minus_W = re.compile('\s[W]\s\=\s{2,5}\-\d{0,4}\.')
-pattern_W = re.compile('\s[W]\s\=\s{2,5}[^-]\d{0,4}\.')
+pattern_W = re.compile('\s[W]\s\=\s{2,5}[-]{0,1}\d{0,4}\.')
 # R:
-pattern_minus_R = re.compile('\s[R]\s\=\s{2,5}\-\d{0,4}\.')
-pattern_R = re.compile('\s[R]\s\=\s{2,5}[^-]\d{0,4}\.')
+pattern_R = re.compile('\s[R]\s\=\s{2,5}[-]{0,1}\d{0,4}\.')
 
 for line in file:
 
     if(re.search(pattern_Y, line)):
-        match = re.search(pattern_Y, line)
-        Y = match[0]
-        index = 28
+        for index, sign in enumerate(line):
+            if sign == 'Y':
+                start_index_pom = index
+                start_index = index + 9
+
         line_list = []
         line_list[:0] = line
-        while index >= 23:
+        while start_index >= start_index_pom:
             if line_list[index].isnumeric():
                 pass
             else:
-                if line_list[index] == ' ':
-                    line_list[index] = '-'
+                if line_list[start_index] == ' ':
+                    line_list[start_index] = '-'
+                    line = ''.join(line_list)
+                    break
+                if line_list[start_index] == '-':
+                    line_list[start_index] = ' '
                     line = ''.join(line_list)
                     break
 
-                if line_list[index] == '-':
-                    line_list[index] = ' '
+            start_index = start_index - 1
+        print(line)
+
+    if(re.search(pattern_R, line)):
+        for index, sign in enumerate(line):
+            if sign == 'R':
+                start_index_pom = index
+                start_index = index + 9
+
+
+        line_list = []
+        line_list[:0] = line
+        while start_index >= start_index_pom:
+            if line_list[index].isnumeric():
+                pass
+            else:
+                if line_list[start_index] == ' ':
+                    line_list[start_index] = '-'
                     line = ''.join(line_list)
                     break
+                if line_list[start_index] == '-':
+                    line_list[start_index] = ' '
+                    line = ''.join(line_list)
+                    break
+            start_index = start_index - 1
 
-            index = index - 1
+    if (re.search(pattern_W, line)):
+        for index, sign in enumerate(line):
+            if sign == 'W':
+                start_index_pom = index
+                start_index = index + 9
+
+        line_list = []
+        line_list[:0] = line
+
+        while start_index >= start_index_pom:
+            if line_list[index].isnumeric():
+                pass
+            else:
+                if line_list[start_index] == ' ':
+                    line_list[start_index] = '-'
+                    line = ''.join(line_list)
+                    break
+                if line_list[start_index] == '-':
+                    line_list[start_index] = ' '
+                    line = ''.join(line_list)
+                    break
+            start_index = start_index - 1
 
         print(line)
 
